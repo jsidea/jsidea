79aa132738c7aed5ac80651d330dc0191da82db6
jsidea <joeran@tzi.de>
2015-09-20 07:01:56 +0200

    arghhh ... SVN vs GIT

--- php/Ressource.php
+++ php/Ressource.php
@@ -1,32 +1,33 @@
 <?php
 class Ressource {
-	public static function importAll($projectName) {
-		echo self::importCSS ( $projectName );
-		echo self::importJavaScript ( $projectName );
-		echo self::importTypeScript ( $projectName );
+	public static function debug($build) {
+		echo self::importCSS ( $build);
+		echo self::importJavaScript ( $build);
+		echo self::importTypeScript ( $build);
 	}
-	public static function importAllMinified($projectName) {
-		echo self::importCSSOnline ( "/eventfive/jsidea/bin/jsidea/css/jsidea.min.css" );
-		echo self::importJavaScriptOnline ( "/eventfive/jsidea/bin/jsidea/js/jsidea.libs.min.js" );
-		echo self::importJavaScriptOnline ( "/eventfive/jsidea/bin/jsidea/js/jsidea.min.js" );
+	public static function build($build) {
+		$projectPathRelative = self::resolveProjectPathRelative ( );
+		echo self::importCSSOnline ( "$projectPathRelative/bin/$build/css/$build.min.css" );
+		echo self::importJavaScriptOnline ( "$projectPathRelative/bin/$build/js/$build.libs.min.js" );
+		echo self::importJavaScriptOnline ( "$projectPathRelative/bin/$build/js/$build.min.js" );
 	}
-	public static function importCSS($projectName, $referencesFileName = "references.css.xml") {
+	private static function importCSS($build) {
 		return self::parseXML ( array (
 				'Ressource',
 				'encodeCSS' 
-		), "CSS", $projectName, $referencesFileName );
+		), "CSS", $build, $build . ".css.xml" );
 	}
-	public static function importJavaScript($projectName, $referencesFileName = "references.javascript.xml") {
+	private static function importJavaScript($build) {
 		return self::parseXML ( array (
 				'Ressource',
 				'encodeJavaScript' 
-		), "JavaScript", $projectName, $referencesFileName );
+		), "JavaScript", $build, $build . ".javascript.xml" );
 	}
-	public static function importTypeScript($projectName, $referencesFileName = "references.typescript.ts") {
-		$result = "<!-- $projectName/TypeScript -->\n";
-		$projectPathRelative = self::resolveProjectPathRelative ( $projectName );
-		$projectPath = self::resolveProjectPathAbsolute ( $projectName );
-		$refs = file_get_contents ( $projectPath . "/" . $referencesFileName );
+	private static function importTypeScript($build) {
+		$result = "<!-- $build/TypeScript -->\n";
+		$projectPathRelative = self::resolveProjectPathRelative (  );
+		$projectPath = self::resolveProjectPathAbsolute (  );
+		$refs = file_get_contents ( $projectPath . "/" . $build . ".typescript.ts" );
 		preg_match_all ( "/path.*\=.*[\"\'].*[\"\']/i", $refs, $out );
 		$res = $out [0];
 		for($i = 0; $i < count ( $res ); ++ $i) {
@@ -38,10 +39,10 @@ class Ressource {
 		}
 		return $result;
 	}
-	public static function importJavaScriptOnline($url, $condition) {
+	private static function importJavaScriptOnline($url, $condition) {
 		return self::wrapCondition ( "<script type='text/javascript' src='$url'></script>\n", $condition );
 	}
-	public static function importCSSOnline($url, $condition) {
+	private static function importCSSOnline($url, $condition) {
 		return self::wrapCondition ( "<link type='text/css' rel='stylesheet' href='$url' >\n", $condition );
 	}
 	private static function encodeCSS($projectPathRelative, $importPath, $condition) {
@@ -50,25 +51,25 @@ class Ressource {
 	private static function encodeJavaScript($projectPathRelative, $importPath, $condition) {
 		return self::wrapCondition ( "<script type='text/javascript' src='$projectPathRelative/libs/$importPath'></script>\n", $condition );
 	}
-	private static function resolveProjectPathAbsolute($projectName) {
-		return realpath ( dirname ( __FILE__ ) . "/../../" . $projectName );
+	private static function resolveProjectPathAbsolute() {
+		return realpath ( dirname ( __FILE__ ) . "/../../jsidea" );
 	}
 	private static function wrapCondition($result, $condition) {
 		if ($condition)
 			return "<![if $condition]>\n$result<![endif]>\n";
 		return $result;
 	}
-	private static function resolveProjectPathRelative($projectName) {
-		$abs = self::resolveProjectPathAbsolute ( $projectName );
+	private static function resolveProjectPathRelative() {
+		$abs = self::resolveProjectPathAbsolute (  );
 		$abs = str_replace ( '\\', '/', $abs );
 		$root = $_SERVER ['DOCUMENT_ROOT'];
 		$root = str_replace ( '\\', '/', $root );
 		return str_replace ( $root, '', $abs );
 	}
-	private static function parseXML($encoderFunction, $type, $projectName, $referencesFileName = "references.css.xml") {
-		$result = "<!-- $projectName/$type -->\n";
-		$projectPathRelative = self::resolveProjectPathRelative ( $projectName );
-		$projectPath = self::resolveProjectPathAbsolute ( $projectName );
+	private static function parseXML($encoderFunction, $type, $build, $referencesFileName) {
+		$result = "<!-- $build/$type -->\n";
+		$projectPathRelative = self::resolveProjectPathRelative ( );
+		$projectPath = self::resolveProjectPathAbsolute ( );
 		$xmlString = file_get_contents ( $projectPath . "/" . $referencesFileName );
 		$scripts = new SimpleXMLElement ( $xmlString );
 		$sources = $scripts->src;
--- php/simbox.php
+++ php/simbox.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /> -->
 <title>jsidea</title>
-<?php include (realpath ( dirname ( __FILE__ ) . '/../php/Ressource.php')); Ressource::importAll ( "jsidea" );?>
+<?php include (realpath ( dirname ( __FILE__ ) . '/../php/Ressource.php')); Ressource::debug ( "jsidea" );?>
 </head>
 <body data-application="jsidea.test.SimboxApplication">
 	<select id="camera-select">
