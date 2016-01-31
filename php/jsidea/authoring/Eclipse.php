8911b817f81dcde3c5f3514c088e6a93d3d579d8
jsidea <joeran@tzi.de>
2016-01-31 02:10:02 +0100

    before adding Eclipse-plugin for Remote-control made easy

---
+++ php/jsidea/authoring/Eclipse.php
@@ -0,0 +1,46 @@
+<?php
+function send_command($command) {
+	$host = "localhost";
+	$port = "3004";
+	$timeout = 15; // timeout in seconds
+	
+	$socket = socket_create ( AF_INET, SOCK_STREAM, SOL_TCP ) or die ( "Unable to create socket\n" );
+	
+	// socket_set_nonblock ( $socket ) or die ( "Unable to set nonblock on socket\n" );
+	
+	set_time_limit ( 0 );
+	
+	$time = time ();
+	if (! @socket_connect ( $socket, $host, $port )) {
+		$err = socket_last_error ( $socket );
+		// if ($err == 115 || $err == 114) {
+		// if ((time () - $time) >= $timeout) {
+		// socket_close ( $socket );
+		// die ( "Connection timed out.\n" );
+		// }
+		// sleep ( 1 );
+		// continue;
+		// }
+		die ( socket_strerror ( $err ) . " - ERRORED\n" . $err );
+	}
+	
+	$in = $command;
+	$file = "G:/software/xampp-portable/htdocs/eventfive/wfb/sixcms_template_checkout_dir/bo-framework/sites/wfb/components/views/wfb_navigation_v1_v_d.cmst";
+	$line_number = 105;
+	$in = array (
+			'plugin' => 'filesystem',
+			'method' => 'openFile',
+			'options' => array (
+					'file' => $file,
+					'line-number' => $line_number 
+			) 
+	);
+	$in = json_encode ( $in );
+	
+	// socket_send($socket, $buf, $len, $flags);
+	socket_write ( $socket, $in, strlen ( $in ) );
+	
+	// socket_set_block ( $this->socket ) or die ( "Unable to set block on socket\n" );
+}
+send_command ( "TEST" );
+?>
